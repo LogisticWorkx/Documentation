4854a3297399d98de90a580ff57d422d4515a1b2
Leon Klasser <leon@logisticworkx.com>
2026-05-11 14:37:01 +0200

    Fix language button

--- shortcuts/generate-pdfs.bat
+++ shortcuts/generate-pdfs.bat
@@ -1,22 +1,24 @@
 @echo off
 cd ..
 
+call :kill_port_8000
+
 echo Building MkDocs site...
 python -m mkdocs build
 if errorlevel 1 goto error
 
-echo Starting MkDocs server silently...
-powershell -NoProfile -Command "Start-Process python -ArgumentList '-m mkdocs serve' -WindowStyle Hidden -PassThru | Select-Object -ExpandProperty Id > .mkdocs-server.pid"
+echo Starting MkDocs server...
+start /B python -m mkdocs serve > .mkdocs-server.log 2>&1
 
 echo Waiting for server...
 timeout /t 5 > nul
 
+echo Generating PDFs...
 node generate-pdfs.js
 if errorlevel 1 goto cleanup_error
 
 echo Stopping MkDocs server...
-for /f %%i in (.mkdocs-server.pid) do taskkill /PID %%i /T /F > nul 2>&1
-del .mkdocs-server.pid > nul 2>&1
+call :kill_port_8000
 
 echo Done!
 pause
@@ -24,13 +26,18 @@ exit /b 0
 
 :cleanup_error
 echo PDF generation failed.
-echo Stopping MkDocs server...
-for /f %%i in (.mkdocs-server.pid) do taskkill /PID %%i /T /F > nul 2>&1
-del .mkdocs-server.pid > nul 2>&1
+call :kill_port_8000
 pause
 exit /b 1
 
 :error
 echo Something failed.
+call :kill_port_8000
 pause
 exit /b 1
+
+:kill_port_8000
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr ":8000" ^| findstr "LISTENING"') do (
+    taskkill /PID %%a /T /F > nul 2>&1
+)
+exit /b 0
